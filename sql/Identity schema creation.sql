efaa70137d70fc3dc20abb9e7d738ee7f4ae9593
Thiago <thiago.grandesso@gmail.com>
2021-02-14 17:59:21 -0300

    Add workflow for continuous deployment set up

--- sql/Identity schema creation.sql
+++ sql/Identity schema creation.sql
@@ -1,92 +1,118 @@
-CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
-                                                       "MigrationId" TEXT NOT NULL CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY,
-                                                       "ProductVersion" TEXT NOT NULL
+IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+CREATE TABLE [__EFMigrationsHistory] (
+                                         [MigrationId] nvarchar(150) NOT NULL,
+                                         [ProductVersion] nvarchar(32) NOT NULL,
+                                         CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
 );
+END;
+GO
 
 BEGIN TRANSACTION;
+GO
 
-CREATE TABLE "AspNetRoles" (
-                               "Id" TEXT NOT NULL CONSTRAINT "PK_AspNetRoles" PRIMARY KEY,
-                               "Name" TEXT NULL,
-                               "NormalizedName" TEXT NULL,
-                               "ConcurrencyStamp" TEXT NULL
+CREATE TABLE [AspNetRoles] (
+                               [Id] nvarchar(450) NOT NULL,
+                               [Name] nvarchar(256) NULL,
+                               [NormalizedName] nvarchar(256) NULL,
+                               [ConcurrencyStamp] nvarchar(max) NULL,
+    CONSTRAINT [PK_AspNetRoles] PRIMARY KEY ([Id])
 );
+GO
 
-CREATE TABLE "AspNetUsers" (
-                               "Id" TEXT NOT NULL CONSTRAINT "PK_AspNetUsers" PRIMARY KEY,
-                               "UserName" TEXT NULL,
-                               "NormalizedUserName" TEXT NULL,
-                               "Email" TEXT NULL,
-                               "NormalizedEmail" TEXT NULL,
-                               "EmailConfirmed" INTEGER NOT NULL,
-                               "PasswordHash" TEXT NULL,
-                               "SecurityStamp" TEXT NULL,
-                               "ConcurrencyStamp" TEXT NULL,
-                               "PhoneNumber" TEXT NULL,
-                               "PhoneNumberConfirmed" INTEGER NOT NULL,
-                               "TwoFactorEnabled" INTEGER NOT NULL,
-                               "LockoutEnd" TEXT NULL,
-                               "LockoutEnabled" INTEGER NOT NULL,
-                               "AccessFailedCount" INTEGER NOT NULL
+CREATE TABLE [AspNetUsers] (
+                               [Id] nvarchar(450) NOT NULL,
+                               [UserName] nvarchar(256) NULL,
+                               [NormalizedUserName] nvarchar(256) NULL,
+                               [Email] nvarchar(256) NULL,
+                               [NormalizedEmail] nvarchar(256) NULL,
+                               [EmailConfirmed] bit NOT NULL,
+                               [PasswordHash] nvarchar(max) NULL,
+    [SecurityStamp] nvarchar(max) NULL,
+    [ConcurrencyStamp] nvarchar(max) NULL,
+    [PhoneNumber] nvarchar(max) NULL,
+    [PhoneNumberConfirmed] bit NOT NULL,
+    [TwoFactorEnabled] bit NOT NULL,
+    [LockoutEnd] datetimeoffset NULL,
+    [LockoutEnabled] bit NOT NULL,
+    [AccessFailedCount] int NOT NULL,
+    CONSTRAINT [PK_AspNetUsers] PRIMARY KEY ([Id])
 );
+GO
 
-CREATE TABLE "AspNetRoleClaims" (
-                                    "Id" INTEGER NOT NULL CONSTRAINT "PK_AspNetRoleClaims" PRIMARY KEY AUTOINCREMENT,
-                                    "RoleId" TEXT NOT NULL,
-                                    "ClaimType" TEXT NULL,
-                                    "ClaimValue" TEXT NULL,
-                                    CONSTRAINT "FK_AspNetRoleClaims_AspNetRoles_RoleId" FOREIGN KEY ("RoleId") REFERENCES "AspNetRoles" ("Id") ON DELETE CASCADE
+CREATE TABLE [AspNetRoleClaims] (
+                                    [Id] int NOT NULL IDENTITY,
+                                    [RoleId] nvarchar(450) NOT NULL,
+                                    [ClaimType] nvarchar(max) NULL,
+    [ClaimValue] nvarchar(max) NULL,
+    CONSTRAINT [PK_AspNetRoleClaims] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles] ([Id]) ON DELETE CASCADE
 );
+GO
 
-CREATE TABLE "AspNetUserClaims" (
-                                    "Id" INTEGER NOT NULL CONSTRAINT "PK_AspNetUserClaims" PRIMARY KEY AUTOINCREMENT,
-                                    "UserId" TEXT NOT NULL,
-                                    "ClaimType" TEXT NULL,
-                                    "ClaimValue" TEXT NULL,
-                                    CONSTRAINT "FK_AspNetUserClaims_AspNetUsers_UserId" FOREIGN KEY ("UserId") REFERENCES "AspNetUsers" ("Id") ON DELETE CASCADE
+CREATE TABLE [AspNetUserClaims] (
+                                    [Id] int NOT NULL IDENTITY,
+                                    [UserId] nvarchar(450) NOT NULL,
+                                    [ClaimType] nvarchar(max) NULL,
+    [ClaimValue] nvarchar(max) NULL,
+    CONSTRAINT [PK_AspNetUserClaims] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_AspNetUserClaims_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
 );
+GO
 
-CREATE TABLE "AspNetUserLogins" (
-                                    "LoginProvider" TEXT NOT NULL,
-                                    "ProviderKey" TEXT NOT NULL,
-                                    "ProviderDisplayName" TEXT NULL,
-                                    "UserId" TEXT NOT NULL,
-                                    CONSTRAINT "PK_AspNetUserLogins" PRIMARY KEY ("LoginProvider", "ProviderKey"),
-                                    CONSTRAINT "FK_AspNetUserLogins_AspNetUsers_UserId" FOREIGN KEY ("UserId") REFERENCES "AspNetUsers" ("Id") ON DELETE CASCADE
+CREATE TABLE [AspNetUserLogins] (
+                                    [LoginProvider] nvarchar(128) NOT NULL,
+                                    [ProviderKey] nvarchar(128) NOT NULL,
+                                    [ProviderDisplayName] nvarchar(max) NULL,
+    [UserId] nvarchar(450) NOT NULL,
+    CONSTRAINT [PK_AspNetUserLogins] PRIMARY KEY ([LoginProvider], [ProviderKey]),
+    CONSTRAINT [FK_AspNetUserLogins_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
 );
+GO
 
-CREATE TABLE "AspNetUserRoles" (
-                                   "UserId" TEXT NOT NULL,
-                                   "RoleId" TEXT NOT NULL,
-                                   CONSTRAINT "PK_AspNetUserRoles" PRIMARY KEY ("UserId", "RoleId"),
-                                   CONSTRAINT "FK_AspNetUserRoles_AspNetRoles_RoleId" FOREIGN KEY ("RoleId") REFERENCES "AspNetRoles" ("Id") ON DELETE CASCADE,
-                                   CONSTRAINT "FK_AspNetUserRoles_AspNetUsers_UserId" FOREIGN KEY ("UserId") REFERENCES "AspNetUsers" ("Id") ON DELETE CASCADE
+CREATE TABLE [AspNetUserRoles] (
+                                   [UserId] nvarchar(450) NOT NULL,
+                                   [RoleId] nvarchar(450) NOT NULL,
+                                   CONSTRAINT [PK_AspNetUserRoles] PRIMARY KEY ([UserId], [RoleId]),
+                                   CONSTRAINT [FK_AspNetUserRoles_AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles] ([Id]) ON DELETE CASCADE,
+                                   CONSTRAINT [FK_AspNetUserRoles_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
 );
+GO
 
-CREATE TABLE "AspNetUserTokens" (
-                                    "UserId" TEXT NOT NULL,
-                                    "LoginProvider" TEXT NOT NULL,
-                                    "Name" TEXT NOT NULL,
-                                    "Value" TEXT NULL,
-                                    CONSTRAINT "PK_AspNetUserTokens" PRIMARY KEY ("UserId", "LoginProvider", "Name"),
-                                    CONSTRAINT "FK_AspNetUserTokens_AspNetUsers_UserId" FOREIGN KEY ("UserId") REFERENCES "AspNetUsers" ("Id") ON DELETE CASCADE
+CREATE TABLE [AspNetUserTokens] (
+                                    [UserId] nvarchar(450) NOT NULL,
+                                    [LoginProvider] nvarchar(128) NOT NULL,
+                                    [Name] nvarchar(128) NOT NULL,
+                                    [Value] nvarchar(max) NULL,
+    CONSTRAINT [PK_AspNetUserTokens] PRIMARY KEY ([UserId], [LoginProvider], [Name]),
+    CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
 );
+GO
 
-CREATE INDEX "IX_AspNetRoleClaims_RoleId" ON "AspNetRoleClaims" ("RoleId");
+CREATE INDEX [IX_AspNetRoleClaims_RoleId] ON [AspNetRoleClaims] ([RoleId]);
+GO
 
-CREATE UNIQUE INDEX "RoleNameIndex" ON "AspNetRoles" ("NormalizedName");
+CREATE UNIQUE INDEX [RoleNameIndex] ON [AspNetRoles] ([NormalizedName]) WHERE [NormalizedName] IS NOT NULL;
+GO
 
-CREATE INDEX "IX_AspNetUserClaims_UserId" ON "AspNetUserClaims" ("UserId");
+CREATE INDEX [IX_AspNetUserClaims_UserId] ON [AspNetUserClaims] ([UserId]);
+GO
 
-CREATE INDEX "IX_AspNetUserLogins_UserId" ON "AspNetUserLogins" ("UserId");
+CREATE INDEX [IX_AspNetUserLogins_UserId] ON [AspNetUserLogins] ([UserId]);
+GO
 
-CREATE INDEX "IX_AspNetUserRoles_RoleId" ON "AspNetUserRoles" ("RoleId");
+CREATE INDEX [IX_AspNetUserRoles_RoleId] ON [AspNetUserRoles] ([RoleId]);
+GO
 
-CREATE INDEX "EmailIndex" ON "AspNetUsers" ("NormalizedEmail");
+CREATE INDEX [EmailIndex] ON [AspNetUsers] ([NormalizedEmail]);
+GO
 
-CREATE UNIQUE INDEX "UserNameIndex" ON "AspNetUsers" ("NormalizedUserName");
+CREATE UNIQUE INDEX [UserNameIndex] ON [AspNetUsers] ([NormalizedUserName]) WHERE [NormalizedUserName] IS NOT NULL;
+GO
 
-INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
-VALUES ('00000000000000_CreateIdentitySchema', '5.0.2');
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20210213143154_Initial Identity Schema changes', N'5.0.2');
+GO
 
 COMMIT;
+GO
