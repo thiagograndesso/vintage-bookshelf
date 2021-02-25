b6da63cde66f2b675c0ea518b85fb73ffbb0e8fe
Thiago Grandesso <thiago.grandesso@gmail.com>
2021-02-25 16:48:57 -0300

    Add CRUD endpoints to BookshelvesController (#5)
    * Add new BookshelvesController with CRUD endpoints
    
    * Use constant variables for Http Status Codes
    
    * Add Produces as application/json media type
    
    * Fix: wrong dependency injection of services
    
    * Fix: wrong mapping DTO type
    
    * Add repository methods to BookshelfRepository
    
    * Update as per PR comments

--- src/VintageBookshelf.Data/Context/VintageBookshelfContext.cs
+++ src/VintageBookshelf.Data/Context/VintageBookshelfContext.cs
@@ -10,7 +10,7 @@ namespace VintageBookshelf.Data.Context
 
         public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
-        public DbSet<Bookshelf> Bookstores { get; set; }
+        public DbSet<Bookshelf> Bookshelves { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
